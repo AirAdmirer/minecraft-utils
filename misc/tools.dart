6e1b876eaee6c3d724a04f747d2989f255f92aeb
Aadhi Arun <88726688+AirAdmirer@users.noreply.github.com>
2022-06-24 23:30:55 +0530

    Create tools.dart

---
+++ misc/tools.dart
@@ -0,0 +1,29 @@
+List<Items> stoneTools = [
+  Items.stoneAxe,
+  Items.stonePickaxe,
+  Items.stoneShovel,
+];
+
+List<Items> woodenTools = [
+  Items.woodenAxe,
+  Items.woodenPickaxe,
+  Items.woodenShovel,
+];
+
+List<Items> ironTools = [
+  Items.ironAxe,
+  Items.ironPickaxe,
+  Items.ironShovel,
+];
+
+List<Items> diamondTools = [
+  Items.diamondAxe,
+  Items.diamondPickaxe,
+  Items.diamondShovel,
+];
+
+List<Items> goldTools = [
+  Items.goldenAxe,
+  Items.goldenPickaxe,
+  Items.goldenShovel,
+];
