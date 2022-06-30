dfbb0387178d047a8662dbab687efcf31d256ab1
Aadhi Arun <88726688+AirAdmirer@users.noreply.github.com>
2022-06-30 17:45:04 +0530

    Create weapons.dart

---
+++ misc/weapons.dart
@@ -0,0 +1,31 @@
+List<Items> woodenTierWeapons = [
+  Items.woodenSword,
+  Items.stonePickaxe,
+  Items.stoneShovel,
+];
+
+List<Items> stoneTierWeapons = [
+  Items.stoneSword,
+  Items.woodenAxe,
+  Items.ironShovel,
+  Items.ironPickaxe,
+];
+
+List<Items> ironTierWeapons = [
+  Items.ironSword,
+  Items.stoneAxe,
+  Items.diamondShovel,
+  Items.diamondPickaxe,
+];
+
+List<Items> diamondTierWeapons = [
+  Items.diamondSword,
+  Items.ironAxe,
+  Items.goldenShovel,
+  Items.goldenPickaxe,
+];
+
+List<Items> goldTierWeapons = [
+  Items.goldenSword,
+  Items.diamondAxe,
+];
